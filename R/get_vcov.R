3e7b8732ac3da34a77c89ee73d26d05ade11be91
François Grolleau <61312833+fcgrolleau@users.noreply.github.com>
2022-12-12 12:05:47 +0100

    fix stacking

--- R/get_vcov.R
+++ R/get_vcov.R
@@ -1,40 +1,38 @@
-get_vcov <- function(data, betas, M){
-
+get_vcov <- function (data, betas, M)
+{
   n <- nrow(data)
   list2env(data, environment())
   list2env(betas, environment())
   betas_names <- names(betas)
-
-  multi_deriv <- function(M_i){
-    lapply(betas_names, function(beta) stats::D(M_i, beta))
+  multi_deriv <- function(M_i) {
+    lapply(betas_names, function(beta) D(M_i, beta))
   }
-
-  Jacob <- lapply(M, multi_deriv)
-
+  jacob <- lapply(M, multi_deriv)
   mid <- sapply(M, function(it) eval(it))
   mid <- as.list(data.frame(t(mid)))
   mid <- lapply(mid, function(x) x %o% x)
-  mid <- Reduce('+', mid)/n
+  mid <- Reduce("+", mid)/n
 
-  comp_jacob <- function(x){
-    sapply(x, function(it) eval(it))
+  correct_len <- function(x) {
+    if(length(x)==1){
+    res <- rep(x, n)
+    } else { res <- x}
+    return(res)
+    }
+
+  comp_jacob <- function(x) {
+    sapply(x, function(it) correct_len(eval(it)) )
   }
-
-  lat <- sapply(Jacob, comp_jacob, simplify = "array")
-  lat <- lapply(seq(dim(lat)[1]), function(x) lat[x, , ])
-  lat <- Reduce('+', lat)/n
-
+  lat <- sapply(jacob, comp_jacob, simplify = "array")
+  lat <- lapply(seq(n), function(x) t(lat[x, , ]) )
+  lat <- Reduce("+", lat)/n
   lat_inv <- solve(lat)
-
   Sigma <- lat_inv %*% mid %*% t(lat_inv)
   vcov <- Sigma/n
   colnames(vcov) <- betas_names
   rownames(vcov) <- betas_names
   se <- sqrt(diag(vcov))
   names(se) <- betas_names
-
-  res <- list(vcov=vcov, se=se, jacob=Jacob)
-
-  return (  res  )
-
+  res <- list(vcov = vcov, se = se, jacob = jacob)
+  return(res)
 }
